cb4da3023960930187ad53f56d105cbb385202f0
Neel Patel <n869p538@pollux.ittc.ku.edu>
2022-02-28 16:04:22 -0600

    10 sec band option

--- iperf_cli.sh
+++ iperf_cli.sh
@@ -3,9 +3,21 @@ export OPENSSL_ENGINES=/home/n869p538/ktls_client_server/openssl/openssl/lib/eng
 export LD_LIBRARY_PATH=/home/n869p538/ktls_client_server/openssl/openssl/lib:/home/n869p538/crypto_mb/2020u3/lib:/home/n869p538/intel-ipsec-mb/lib
 export LD_LIBRARY_PATH=/home/n869p538/ktls_client_server/openssl/openssl/lib:/home/n869p538/crypto_mb/2020u3/lib:/home/n869p538/intel-ipsec-mb/lib
 if [ "${1}" = "tcpw" ]; then 
-	taskset 0x2000000000 ./src/iperf -c 192.168.1.1 -t 60 -i 5 -y C > tcp_througput.csv
+	if [ "${2}" = "--band" ]; then
+		taskset 0x2000000000 ./src/iperf -c 192.168.1.1 -t 10
+	else
+		taskset 0x2000000000 ./src/iperf -c 192.168.1.1 -t 60 -i 5 -y C > tcp_througput.csv
+	fi;
 elif [ "${1}" = "tlsq" ]; then
-	taskset 0x2000000000 ./src/iperf --tls=qat -c 192.168.1.1 -t 60 -i 5 -y C > tls_offload_throughput.csv
+	if [ "${2}" = "--band" ]; then
+		taskset 0x2000000000 ./src/iperf --tls=qat -c 192.168.1.1 -t 10
+	else
+		taskset 0x2000000000 ./src/iperf --tls=qat -c 192.168.1.1 -t 60 -i 5 -y C > tls_offload_throughput.csv
+	fi;
 elif [ "${1}" = "tls" ]; then
-	taskset 0x2000000000 ./src/iperf --tls=v1.2 -c 192.168.1.1 -t 60 -i 5 -y C > tls_default_throughput.csv
+	if [ "${2}" = "--band" ]; then
+		taskset 0x2000000000 ./src/iperf --tls=v1.2 -c 192.168.1.1 -t 10
+	else
+		taskset 0x2000000000 ./src/iperf --tls=v1.2 -c 192.168.1.1 -t 60 -i 5 -y C > tls_default_throughput.csv
+	fi;
 fi
